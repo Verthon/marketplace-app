4bad9a5add7d65cd55f664fe5010ea9f68330747
Krzysztof Sordyl <krzysztof.sordyl@tsh.io>
2020-09-28 19:59:10 +0200

    feat: add date handling

--- components/Advert/AdvertCard/AdvertCard.tsx
+++ components/Advert/AdvertCard/AdvertCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import dayjs from 'dayjs'
 
 import { Styled } from './AdvertCard.styles'
 import { Props } from './AdvertCard.types'
@@ -19,6 +20,10 @@ export const AdvertCard = ({ advert }: Props) => {
             <div>
               <Styled.Title>{advert.title}</Styled.Title>
               <Styled.Price>{advert.priceValue}</Styled.Price>
+            </div>
+            <div>
+              <Styled.Location>{advert.contact_city}</Styled.Location>
+              <Styled.Date>{dayjs(advert.created_at).format('DD MMMM YYYY')}</Styled.Date>
             </div>
           </Styled.Content>
         </Styled.InnerWrapper>
